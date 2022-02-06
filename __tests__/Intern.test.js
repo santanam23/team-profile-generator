f240f1f71333ce2f113efb0d85f640dece3f11c1
santanam23 <mirmir11.ms@gmail.com>
2022-02-06 17:44:18 -0600

    passed test

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,23 @@
+// using Intern constructor 
+const Intern = require('../lib/Intern');
+
+// creating intern object  
+test('creates an Intern object', () => {
+    const intern = new Intern('Miriam', 90, 'miriam.steph.santana@gmail.com', 'RiceUniversity');
+    
+    expect(intern.school) .toEqual(expect.any(String));
+});
+
+// gets school from getSchool()
+test('gets employee school', () => {
+    const intern = new Intern('Miriam', 90, 'miriam.steph.santana@gmail.com', 'RiceUniversity');
+    
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const intern = new Intern('Miriam', 90, 'miriam.steph.santana@gmail.com.com', 'RiceUniversity');
+
+    expect(intern.getRole()).toEqual("Intern");
+}); 
