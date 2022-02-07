d63686afb0b0fec21acb2d1cd9f17d8e24b03de1
santanam23 <mirmir11.ms@gmail.com>
2022-02-06 18:07:19 -0600

    tested node index.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,21 +3,21 @@ const Intern = require('../lib/Intern');
 
 // creating intern object  
 test('creates an Intern object', () => {
-    const intern = new Intern('Miriam', 90, 'miriam.steph.santana@gmail.com', 'RiceUniversity');
+    const intern = new Intern('Miriam', 900, 'miriam.steph.santana@gmail.com', 'RiceUniversity');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
 
 // gets school from getSchool()
 test('gets employee school', () => {
-    const intern = new Intern('Miriam', 90, 'miriam.steph.santana@gmail.com', 'RiceUniversity');
+    const intern = new Intern('Miriam', 900, 'miriam.steph.santana@gmail.com', 'RiceUniversity');
     
     expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const intern = new Intern('Miriam', 90, 'miriam.steph.santana@gmail.com.com', 'RiceUniversity');
+    const intern = new Intern('Miriam', 900, 'miriam.steph.santana@gmail.com.com', 'RiceUniversity');
 
     expect(intern.getRole()).toEqual("Intern");
 }); 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -3,14 +3,14 @@ const Manager = require('../lib/Manager');
 
 // creating manager object  
 test('creates an Manager object', () => {
-    const manager = new Manager('Miriam', 90, 'miriam.steph.santana@gmail.com', 4);
+    const manager = new Manager('Miriam', 900, 'miriam.steph.santana@gmail.com', 4);
     
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 // gets role from getRole()
 test('gets role of employee', () => {
-    const manager = new Manager('Miriam', 90, 'miriam.steph.santana@gmail.com.');
+    const manager = new Manager('Miriam', 900, 'miriam.steph.santana@gmail.com.');
 
     expect(manager.getRole()).toEqual("Manager");
 }); 
